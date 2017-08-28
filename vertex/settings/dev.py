d1cda50d2f7338b46cf12c67176710c0efcf9ef2
Jonathan Senecal <jonathan@zap.coop>
2017-08-28 16:40:44 -0400

    Fix settings to make them modular

--- vertex/settings/dev.py
+++ vertex/settings/dev.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 INSTALLED_APPS += ('debug_toolbar',
                    'django_extensions',)
 
-MIDDLEWARE_CLASSES = ('debug_toolbar.middleware.DebugToolbarMiddleware',) + MIDDLEWARE_CLASSES
+MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware',] + MIDDLEWARE
 
 DATABASES = {
     'default': {
@@ -26,25 +26,3 @@ DATABASES = {
         'HOST': '127.0.0.1',
     },
 }
-
-CACHES['default']['LOCATION'] = '/var/run/redis/redis.sock'
-
-JWT_AUTH['JWT_EXPIRATION_DELTA'] = datetime.timedelta(seconds=259200)
-JWT_AUTH['JWT_ALGORITHM'] = 'HS256'
-
-
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = 'lmail'
-EMAIL_HOST_PASSWORD = 'badpassword'
-
-
-# Celery
-BROKER_URL = "amqp://noss:hlgVSjikAJQTKH2muZ1L@localhost:5672/noss"
-
-APP_ALLOW_UNVERIFIED_STAFF = True
-
-WS_AMQP_CONNECTION = {'host': 'localhost',
-                      'userid': 'noss',
-                      'password': 'hlgVSjikAJQTKH2muZ1L',
-                      'virtual_host': 'noss'}
-
