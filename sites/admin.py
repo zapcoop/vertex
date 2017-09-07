83af5b9e232710621dce8cdad6bf6aaf07e4112d
Jonathan Senecal <jonathan@zap.coop>
2017-09-07 14:11:44 -0400

    Working on Sites

--- sites/admin.py
+++ sites/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from sites.models import (
+    Place,
+    AddressComponentThrough
+)
+
+
+class AddressComponentInline(admin.TabularInline):
+    model = AddressComponentThrough
+    extra = 0
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = (AddressComponentInline,)
+    readonly_fields = ('lat_long', 'place_type')
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # This is the case when obj is already created i.e. it's an edit
+            return self.readonly_fields + ('raw_address', 'google_place_id')
+        else:
+            return self.readonly_fields
+
+
+admin.site.register(Place, PlaceAdmin)
