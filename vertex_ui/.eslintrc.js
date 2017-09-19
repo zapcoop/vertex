9380580c56ae04a7aef09bc37bf87a0494c1493e
Jonathan Senecal <jonathan@zap.coop>
2017-09-19 14:13:52 -0400

    Getting there

---
+++ vertex_ui/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['eslint-config-react-app', 'prettier'],
+  plugins: ['prettier',],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
--- vertex_ui/src/index.js
+++ vertex_ui/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+(function() {
+  let bodyElement = document.querySelector('body');
+  bodyElement.classList.add('loading');
 
+  document.addEventListener('readystatechange', function() {
+    if (document.readyState === 'complete') {
+      let bodyElement = document.querySelector('body');
+      let loaderElement = document.querySelector('#initial-loader');
+
+      bodyElement.classList.add('loaded');
+      setTimeout(function() {
+        bodyElement.removeChild(loaderElement);
+        bodyElement.classList.remove('loading', 'loaded');
+      }, 200);
+    }
+  });
+})();
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
