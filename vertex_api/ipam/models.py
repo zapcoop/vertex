327eab8f371e3aec70a8b0c059fe11554071c18b
Jonathan Senecal <jonathan@zap.coop>
2017-11-17 11:07:52 -0500

    Working on ipam models

--- vertex_api/ipam/models.py
+++ vertex_api/ipam/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# IP Address
+# IP Range
+# Subnet/Prefix
+# VRF
+
+# Fabric
+# VLan
+# Space
+
+# VLAN Group
+
+# Interface / Physical
+# Interface / Bond
+# Interface / VLAN
+
+# MAC Address
+
+# Roles
