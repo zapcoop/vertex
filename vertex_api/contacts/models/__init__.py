9cb9985089e25c3a87c9782971d317f6221b478b
Jonathan Senecal <jonathan@zap.coop>
2018-01-19 12:26:04 -0500

    Missing Import

--- vertex_api/contacts/models/__init__.py
+++ vertex_api/contacts/models/__init__.py
@@ -5,5 +5,5 @@ from .emailaddress import EmailAddress
 from .phonenumber import PhoneNumber
 from .url import URL
 from .place import Place
-from .organization import Organization, OrganizationAlias, Manufacturer, HardwareSupplier
+from .organization import Organization, OrganizationAlias, Manufacturer, HardwareSupplier, TelecomProvider
 from .person import PeopleOrganizations, Person
